20bd0ce77aec7c98240044d7534ea7c77f68ceae
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-10-25 19:30:13 -0400

    changed location variable to a list and wrote descriptions for each

--- TextGame.py
+++ TextGame.py
@@ -2,6 +2,7 @@
 #Author: Daniel Gisolfi
 #Date: 9/22/16
 #TextGame.py
+#Version 0.3
 
 def game():
     #Start Game
@@ -9,29 +10,64 @@ def game():
     title = "The Abandoned Lab:"
     print(title)
     playername = input("Enter the name of your player: ")
-    backstory = playername + (" has arrived at a large metal door\n"
-                 "leading to an abandoned Computer labratory, explore"
-                 "\ninside to discover new technology")
+    backstory = playername + (", you are lost in the woods and searching for any relief from civilization"
+                            +" you come across what used to be a scientific laboratory researching new technologies"
+                            +" in the side of a mountain. The lab appears abandoned but could hold the key to surviving" 
+                            +" nature…New technology!!! Explore inside to find some. \n")
+                 
     score = int(0)
-    ##Game Locations   
-    
-    labentrance = "You are at the Lab entrance"
-    servicedesk =  "You are at the Service Desk"
-    powercenter = "You are at the Power Center"
-    computerlab = "You are in the Computer Lab"
-    datacenter =  "You are in the Data Center"
-    
 
-    ###Locations Visited
-    labentranceVisited = True
-    servicedeskVisted = False
-    computerlabVisted = False
-    datacenterVisted = False
-    powercenterVisited = False
+    ##Game Locations   
+    allLoc = ["labentrance","servicedesk", "powercenter","Storage room", "computerlab", "datacenter","Testing Room"]
+
+    ###Location Descriptions
+    locDetails = [
+    #Lab Entrance
+    ("You come to the entrance where a large metal door used to stand protecting the lab and the nerds inside from the"
+    +" wilderness. There is now just a large opening leading into the dark lab. Head inside to further explore the lab")
+    #servicedesk
+    (" Entering the lab there is a service desk waiting dead ahead dimly lit only by the flickering CRT monitors left"
+    +" behind. there seems to be nothing here but unless computers from the 90s and lots of crumpled papers.")
+    #powercenter
+    ("Huge generators fill the room, they are all shutdown as the main power switch is turned off,"
+    +" the backup battery seems to be charged and is the only source of power for the lab currently")
+    #Storage room
+    ("Behind the door to the storage room lies shelves upon shelves of prtotypes of jetpacks, flying cars and hoverboards")
+    #Computerlab
+    ("In the computer lab there is little walking space as the room was stuffed to the max with as many cubicles that"
+    +" would fit. There is a lone ceiling fan missing half its blades with plenty of trashed computers, paper and floppy"
+    +" disks scattered across the room.")
+    #datacenter
+    ("Large mainframes hold mass amounts of data collected by the lab while it was operational,"
+    +" including all reports of experiments and tests")
+    #Testing Room
+    ("In this room lies the finished technologies of the lab that where left, you see dozons of itmes that untill now only"
+    +" seemed science fiction, including a small mech suit which you can use to get back to civilization.")
+    ] 
+
+
+    ####Locations Visited
+
+    locVisit = []
+
+    '''Map
+                                        Testing Room
+                                            |
+                                            |
+                                        Datacenter
+                        Storage Room        |
+                             |              |
+                             |              |
+         Powercenter-----Servicedesk----Computer lab
+                             |   
+                             |
+                             |
+                        Lab Entrance
+
+    '''
         
     #Game Start
     playerlocation = labentrance
-    
     
     print()
     print(backstory)
@@ -40,8 +76,8 @@ def game():
 
     #Game loop
     while True:
-        userinput = input("Which direction would you like to go?: \n")
-        userinput = userinput.lower()
+        cmd = input("Which direction would you like to go?: \n")
+        cmd = userinput.lower()
 
         if userinput == "north":
             if playerlocation == labentrance:
@@ -64,6 +100,7 @@ def game():
                 break
             elif playerlocation == datacenter:
                 print("there is a wall there")
+        
         elif userinput == "south":
             
             if playerlocation == labentrance:
