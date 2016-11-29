130f46ccc6c2ea2e24dfaaf34044caae30a21f4c
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-11-28 22:24:47 -0500

    started converting game to class structure

--- TextGame.py
+++ TextGame.py
@@ -1,48 +1,66 @@
 #Into to programing
 #Author: Daniel Gisolfi
-#Date: 11/4/16
+#Date: 11/21/16
 #TextGame.py
-#Version 0.7
+#Version 0.9
+
+
+class Player
+    
+    maxItems = 5
+    #constructor
+    def __init__(self,name):
+        self.name = name    #atrributes of player
+        self.location = 0
+        self.score = 0
+        self.inventory = []
+        self.moveCount = 0
+
+    #method - an operation we an preform on a player
+    def take(self, item):
+        inventory.append(itemList[currLoc])
+        print("you now have a", itemList[currLoc])
+        itemList[currLoc] = "nothing"
+
+        if len(self.inventory) < Player.maxItems:
+            self.inventory.append(item)
+            return True
+        else:
+            return False
+
+    def drop(self, item):
+        pass
+
+    def Examine(self, item):
+        if itemList[currLoc] != "nothing": #checks if the current location matches any locations in the items list
+            print("There is a", itemList[currLoc], "in the area" )#reveal
+        else:
+            print("there is no item in this area")#let the player know there is not a item in this area
+
+class Locale
+
+    def __init__(self)
+        self.locations = []
+        self.
 
 
 
 
-#Game Locations 
-allLoc = ["labentrance","servicedesk", "powercenter", "sideentrance""Storage room", "computerlab", "datacenter","testingroom"] 
-visited = [True,False,False,False,False,False,False, False]
-locDetails = [
-#Lab Entrance
-("You come to the entrance where a large metal door used to stand protecting the lab and the nerds inside from the"
-+" wilderness. There is now just a large opening leading into the dark lab. Head inside to further explore the lab"),
-#servicedesk
-("Entering the lab there is a service desk waiting dead ahead dimly lit only by the flickering CRT monitors left"
-+" behind. there seems to be nothing here but unless computers from the 90s and lots of crumpled papers."),
-#powercenter
-("Huge generators fill the room, they are all shutdown as the main power switch is turned off,"
-+" the backup battery seems to be charged and is the only source of power for the lab currently"),
-#Storage room
-("Behind the door to the storage room lies shelves upon shelves of prtotypes of jetpacks, flying cars and hoverboards"),
-#Computerlab
-("In the computer lab there is little walking space as the room was stuffed to the max with as many cubicles that"
-+" would fit. There is a lone ceiling fan missing half its blades with plenty of trashed computers, paper and floppy"
-+" disks scattered across the room."),
-#datacenter
-("Large mainframes hold mass amounts of data collected by the lab while it was operational,"
-+" including all reports of experiments and tests"),
-#Testing Room
-("In this room lies the finished technologies of the lab that were left, you see dozons of itmes that untill now only"
-+" seemed science fiction, including a small mech suit which you can use to get back to civilization.")]
+
 
 #Game Matrix
 
-gameMatrix =[[1,-1,-1,3],
+gameMatrix =[
+[1,-1,-1,3],
 [4,0,5,2],
 [-1,3,1,-1],
 [2,-1,0,-1],
 [-1,1,6,-1],
 [6,-1,-1,1],
 [7,5,-1,4],
-[-1,6,-1,-1]]
+[-1,6,-1,-1],
+[-1,2,4,-1],
+[-1,4,7 ,-1]]
 
 
 #item list
@@ -109,22 +127,14 @@ def getDestination(startloc,direct):
     dest = gameMatrix[currLoc][direct]
     print(dest)
     if dest == nowhere:
-            print("you cannot go",cmd,"from",locationNames[startloc],".")
+            print("you cannot go",cmd,"from",allLoc[startloc],".")
             dest = startloc
             return dest
     move(dest)
-
-def take():
-    inventory.append(itemList[currLoc])
-    print("you now have a", itemList[currLoc])
-    itemList[currLoc] = "nothing"
-
+    
 
 def examine():
-    if itemList[currLoc] != "nothing": #checks if the current location matches any locations in the items list
-        print("There is a", itemList[currLoc], "in the area" )#reveal
-    else:
-        print("there is no item in this area")#let the player know there is not a item in this area
+    
 
 
 def updateGame():
@@ -137,7 +147,7 @@ def updateGame():
     print()
     
     
-def userinput():
+def  loop():
     while True:
         global cmd
         cmd = input("what would you like to do?: \n")
@@ -153,9 +163,9 @@ def userinput():
             direct = 3  
 
         elif cmd == "examine":
-            print("dude it worked")
             examine()
             continue
+
         elif cmd == "take":
             take()
             continue
@@ -174,17 +184,19 @@ def userinput():
         elif cmd == "map":
             if "map" in inventory:
                 print('''Map
-                                Testing Room(7)
-                                    |
-                                    |
-            Storage Room(4)-----Datacenter(6)
+
+
+            Manufacuring Lab(8) Testing Room(7)
                      |              |
                      |              |
-                     |              |
+Network Room--Storage Room(4)-----Datacenter(6)
+      |              |              |
+      |              |              |
+      |              |              |
 Powercenter(2)-----Servicedesk(1)---Computer lab(5)
-  |                  |   
-  |                  |
-  |                  |
+      |              |   
+      |              |
+      |              |
 Side Entrance(3)---Lab Entrance(0)
 
 ''')
@@ -199,12 +211,12 @@ Side Entrance(3)---Lab Entrance(0)
         getDestination(currLoc,direct)
 #End Game
 def ending():
-    if moveCount = 15
+    if moveCount == 15:
         print("you ran out of moves")
         print()
         print("Game Over")
         print()
-    elif currLoc == 7
+    elif currLoc == 7:
         conclusion = "Congratulations " + playername + ", you found the Testing room and technology inside, to help you get home safe"
         print(conclusion)
         print() 
